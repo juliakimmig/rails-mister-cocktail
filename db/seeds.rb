7cd60da85283c456649b12a1164319ddbea33c80
Julia Kimmig <juliakimmig@gmail.com>
2019-03-29 10:41:38 +0800

    ...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+
+result = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+drinks = result['drinks']
+
+drinks.each do |hash|
+  # create loop to add all ingredients in db, drinks is array, ingredients are hashes
+  Ingredient.create!(name: hash['strIngredient1'])
+end
+
+result = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail').read)
+cocktails = result['drinks']
+
+cocktails.each do |hash|
+  # create loop to add all ingredients in db, drinks is array, ingredients are hashes
+  Cocktail.create!(name: hash['strDrink'], image: hash['strDrinkThumb'])
+end
+
+
+description = ["1oz", "2oz", "4oz"]
+
+10.times do
+  Dose.create!(description: description.sample, ingredient: Ingredient.all.sample, cocktail: Cocktail.all.sample)
+end
+
+# photo: hash['strDrinkThumb']
